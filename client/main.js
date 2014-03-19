cb045921107b2df6dc683d962c3cdef46020ccaa
deanwenick <deanwenick@gmail.com>
2014-03-19 12:51:32 -0700

    added accounts packages and login template to header, moved header from layout template

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 //this subscription is now handled by router in waitOn method
 //Meteor.subscribe( 'posts' );
+
+//works when header with pageTitle is in layout.html, not in header.html
 Meteor.startup(function() {
     Session.set('pageTitle', "Microscope");
 });
