9679361d65d619b2145dcc00ba3aab9de9ebe3f3
deanwenick <deanwenick@gmail.com>
2014-03-20 16:12:43 -0700

    added post edit page and favicon

---
+++ client/views/posts/post_edit.js
@@ -0,0 +1,31 @@
+Template.postEdit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var currentPostId = this._id;
+
+        var postProperties = {
+            title: $(e.target).find( '[name=title]' ).val(),
+            author: $(e.target).find( '[name=author]' ).val(),
+            url: $(e.target).find( '[name=url]' ).val()
+        };
+
+        Posts.update(currentPostId, {$set: postProperties}, function (e, error) {
+            if (error) {
+                alert(error.reason);
+            } else {
+                router.go('postPage', {_id: currentPostId});
+            }
+        });
+    },
+
+    'click delete': function (e) {
+        e.preventDefault();
+
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            router.go('postsList');
+        }
+    }
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,18 +2,21 @@ if (Posts.find().count() === 0) {
     Posts.insert({
         title: 'Introducing Telescope',
         author: 'Sacha Grief',
-        url: 'http://sachgreif.com/introducing-telescope/'
+        url: 'http://sachgreif.com/introducing-telescope/',
+        contributer: 'The Author'
     });
 
     Posts.insert({
         title: 'Meteor',
         author: 'Tom Coleman',
-        url: 'http://meteor.com'
+        url: 'http://meteor.com',
+        contributer: 'The Author'
     });
 
     Posts.insert({
         title: 'The Meteor Book',
         author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
+        url: 'http://themeteorbook.com',
+        contributer: 'The Author'
     });
 }
