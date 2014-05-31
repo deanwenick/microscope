906cc514ec3d958f433f71ac2e815067673f58d7
deanwenick <deanwenick@gmail.com>
2014-05-31 14:26:22 -0700

    only sync notifications that are relevant to the user

--- server/publications.js
+++ server/publications.js
@@ -16,6 +16,6 @@ Meteor.publish( 'comments', function(postId) {
 });
 
 Meteor.publish('notifications', function() {
-    return Notifications.find();
+    return Notifications.find({userId: this.userId});
 });
 
